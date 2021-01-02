7495b09c471669ef52174cd9b331f90daf33df76
temp <as98742050@gmail.com>
2021-01-02 17:31:56 +0530

    modified

--- prac2/app/src/main/java/com/example/prac2/MainActivity.java
+++ prac2/app/src/main/java/com/example/prac2/MainActivity.java
@@ -23,20 +23,21 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-
+    int quantity=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+
     /**
      * This method is called when the order button is clicked
      */
     public void submitOrder(View view) {
-        int numberOfCoffees=2;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees*5);
+
+
+        displayPrice(quantity*5);
     }
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
@@ -50,4 +51,24 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+    /**
+     * This method is called when the +button is clicked
+     */
+
+    public void increment(View view) {
+
+         quantity=quantity+1;
+        display(quantity);
+
+    }
+    /**
+     * This method is called when the - button is clicked
+     */
+    public void decrement(View view) {
+
+        display(--quantity);
+
+    }
+
+
 }
