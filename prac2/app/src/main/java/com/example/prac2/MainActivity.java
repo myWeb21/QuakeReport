9a0174121f34bd050af2d72c7cbfdc7d754108af
temp <as98742050@gmail.com>
2021-02-04 17:02:54 +0530

    initial

--- prac2/app/src/main/java/com/example/prac2/MainActivity.java
+++ prac2/app/src/main/java/com/example/prac2/MainActivity.java
@@ -24,6 +24,7 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
     int quantity=0;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,16 +40,23 @@ public class MainActivity extends AppCompatActivity {
 
         displayPrice(quantity*5);
     }
+
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        priceTextView.setText("name:\nnwhipped cream:\n quantity"+quantity+"\nthank you\nprice "+NumberFormat.getCurrencyInstance().format(number));
     }
+    public void display()
+    {
+
+    }
+
 
     /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+
         quantityTextView.setText("" + number);
     }
     /**
